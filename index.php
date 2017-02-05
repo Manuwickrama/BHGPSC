16a9014abee6cf8f0fd0e52f856ca7aa968e1d23
Manuwickrama <manuwickrama@yahoo.com>
2017-02-05 22:36:14 +1030

    Commit 4

--- index.php
+++ index.php
@@ -11,11 +11,14 @@
 </head>
 <body id="main" onload="">
 <form id="form1" name="form1" method="POST" enctype="multipart/form-data" action="">
+
+	<!-- Manu: All Input tags need a name like below and respective php tags. -->
 	<input type="text" name="2017.Jan.GPSC" value="<?php put(2017,Jan,GPSC); ?>">
 	<input type="text" name="2016.Jan.GPSC" value="<?php put(2016,Jan,GPSC); ?>">
 	<input type="text" name="2015.Jan.GPSC" value="<?php put(2015,Jan,GPSC); ?>">
 	<input type="text" name="2014.Jan.GPSC" value="<?php put(2014,Jan,GPSC); ?>">
-	
+	<input type="text" name="2013.Jan.GPSC" value="<?php put(2013,Jan,GPSC); ?>">
+
 	<!-- Manu: This input is hidden. Please add this to the bottom of the form. -->
 	<input type="submit" name="" value="submit" hidden>
 
