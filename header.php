22c1492531f1926788527b8f98366a597cb875e8
Manuwickrama <localadmin@BHGPSC-Manu.local>
2017-02-09 08:30:18 +1030

    Added support for 2 line plots

---
+++ header.php
@@ -0,0 +1,64 @@
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Vapour Statistics</a>
+            </div>
+            <div class="navbar-pagetitle">
+                <h3>DASHBOARD
+                <small>Overview</small></h3>
+            </div>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <hr>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#bhgpsc"><i class="fa fa-fw fa-arrows-v"></i> BHGPSC <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="bhgpsc" class="collapse">
+                            <li><a href="new-pt-bhgpsc.html">New Patient Statistics</a></li>
+                            <li><a href="ob-stats-bhgpsc.html">Online Booking Statistics</a></li>
+                            <li><a href="shs-bhgpsc.html">Shared Health Summary Stats</a></li>
+                            <li><a href="tap-bhgpsc.html">Total Active Patients</a></li>
+                        </ul>
+                    </li><!-- bhgpsc -->
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#smc"><i class="fa fa-fw fa-arrows-v"></i> SMC <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="smc" class="collapse">
+                            <li><a href="new-pt-smc.html">New Patient Statistics</a></li>
+                            <li><a href="ob-stats-smc.html">Online Booking Statistics</a></li>
+                            <li><a href="shs-smc.html">Shared Health Summary Stats</a></li>
+                            <li><a href="tap-smc.html">Total Active Patients</a></li>
+                        </ul>
+                    </li><!-- smc -->
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#combined"><i class="fa fa-fw fa-arrows-v"></i> Combined <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="combined" class="collapse">
+                            <li><a href="new-pt-comb.html">New Patient Statistics</a></li>
+                            <li><a href="ob-stats-comb.html">Online Booking Statistics</a></li>
+                            <li><a href="shs-comb.html">Shared Health Summary Stats</a></li>
+                            <li><a href="tap-comb.html">Total Active Patients</a></li>
+                        </ul>
+                        <hr>
+                    </li><!-- combined -->
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#admin"><i class="fa fa-fw fa-user"></i> Admin <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="admin" class="collapse">
+                            <li><a href="new-pt-admin.html">New Patient Statistics</a></li>
+                            <li><a href="ob-stats-admin.html">Online Booking Statistics</a></li>
+                            <li><a href="shs-admin.html">Shared Health Summary Stats</a></li>
+                            <li><a href="tap-admin.html">Total Active Patients</a></li>
+                        </ul>
+                    </li><!-- admin -->
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
